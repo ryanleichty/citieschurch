69866a2c0a4da3d12d5ffde001a763a045f3cc36
Ryan Leichty <ryan.leichty@gmail.com>
2016-05-11 18:03:06 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -476,6 +476,6 @@ $(document).ready(function() {
 
 // Remove Indent on New Paragraphs
 
-		$( "p:contains(' ') + p" ).css( "color", "blue" );
+		$( "p + p:contains(' ')" ).css( "color", "blue" );
 
 })
